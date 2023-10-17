2541e132bc5225fc84fa92e0decd3ce0d456f5f9
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-10-17 19:49:56 +0200

    feature/ add values seeder Books

--- database/seeders/BooksTableSeeder.php
+++ database/seeders/BooksTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -15,6 +16,16 @@ class BooksTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        //
+
+        for ($i=0; $i < 10; $i++) { 
+            $newBook = new Book();
+            $newBook->ISBN= $faker->numerify("#############");
+            $newBook->name= $faker->word();
+            $newBook->author= $faker->words(2, true);
+            $newBook->type= $faker->randomElement(["sci-fi", "horror", "fantasy", "thriller"]);
+            $newBook->description= $faker->text(100);
+            $newBook->save();
+        }
+        
     }
 }
