9f30b402d067647464056266b8c7aad607f63b3e
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-10-17 16:16:22 +0200

    add migrations books

---
+++ database/migrations/2023_10_17_140127_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('ISBN');
+            $table->string("name", 25);
+            $table->string("author", 50);
+            $table->enum("type", ["sci-fi", "horror", "fantasy", "thriller"]);
+            $table->text("description");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
