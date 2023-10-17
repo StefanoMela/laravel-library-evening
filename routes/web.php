b4869d6d915c4213f838edd0b7e80bbc6f8864d5
Catiflam <gilfrid1@gmail.com>
2023-10-17 16:36:06 +0200

    add  feature controller book

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\PageController;
 
 use Illuminate\Support\Facades\Route;
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'index'])->name('home');
+
+Route::get('/book',[BookController::class,'index'])->name('book.index');
