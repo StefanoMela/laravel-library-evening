e89da6ae4de0c20b0765706d01b0a204da8476d4
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-10-20 17:11:34 +0200

    add_delete

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('main-content')
 <section class="container mt-5">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
   @forelse($books as $book)
     <p>
@@ -9,6 +10,16 @@
       <strong>Author</strong>: {{ $book->author }} <br>
       <strong>Genere</strong>: {{ $book->type }}
       <strong>Description</strong>: {{ $book->description }} <br>
+
+      <form action="{{route('books.destroy',$book)}}" method="POST">
+        @csrf
+        @method('DELETE')
+<button class="trash">
+<i class="fa-solid fa-trash text-danger"></i>
+
+</button>
+
+    </form>
     </p>
     <hr>
   @empty
