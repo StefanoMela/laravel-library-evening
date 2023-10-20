0abe9d55a4fa9b67ef2516f5529614a5843329b3
StefanoMela <stefano.mela25@gmail.com>
2023-10-20 16:13:51 +0200

    small fix

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,9 +7,81 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
         $books = Book::all();
         return view('book.index', compact('books'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    public function show(Book $book)
+    {
+        return view('books.show', compact('book'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Book $book)
+    {
+        return view('books.edit', compact('book'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Book $book)
+    {
+        $data = $request->all();
+        $book->update($data);
+        return redirect()->route('books.show', $book);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return redirect()->route('books.index');
     }
 }
