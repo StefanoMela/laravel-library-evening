44aa285285dd60b8107c340c31a9daaeef89ae6a
StefanoMela <stefano.mela25@gmail.com>
2023-11-10 17:04:47 +0100

    feat/ book-index-api-done

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Book extends Model
 {
     use HasFactory;
-    public function genre()  {
+    public function genre()
+    {
         return $this->belongsTo(Genre::class);
     }
 
@@ -20,22 +22,24 @@ class Book extends Model
         "description",
         "genre_id",
         'book_cover'
-    ] ;
+    ];
     // public function getGenre() {
     //     return $this->genre->name;
     // }
 
-    public function tags() {
+    public function tags()
+    {
         return $this->belongsToMany(Tag::class);
     }
 
-    public function getTagBadges(){
+    public function getTagBadges()
+    {
         $_tags = "";
 
-        if($this->tags->toArray()){
-            foreach($this->tags as $tag){
+        if ($this->tags->toArray()) {
+            foreach ($this->tags as $tag) {
                 $_tags .= "<span class='badge mx-1' style='background-color:{$tag->color}'>{$tag->label}</span>";
-            }  
+            }
         } else {
             $_tags .= "Senza tags";
         }
@@ -44,4 +48,13 @@ class Book extends Model
         return $_tags;
     }
 
+    public function getDescriptionIndex($chars = 50)
+    {
+        return strlen($this->description) > $chars ? substr($this->description, 0, $chars) . "..." : $this->description;
+    }
+
+    public function getAbsUriImage()
+    {
+        return $this->book_cover ? Storage::url($this->book_cover) : null;
+    }
 }
